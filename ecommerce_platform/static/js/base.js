921c185d67fd274aab0928782f89b7c2c75f7111
aiyana <xinyih@andrew.cmu.edu>
2021-03-30 13:46:11 -0700

    Added basic HTML for Wishlist and Shopping Cart Page

--- ecommerce_platform/static/js/base.js
+++ ecommerce_platform/static/js/base.js
@@ -1,3 +1,9 @@
 function nav_drop_down(){
     document.getElementById("id_dropdown_content").classList.toggle("show");
 }
+
+function toggle(source) {
+    checkboxes = document.getElementsByName('available_product');
+    for(var checkbox in checkboxes)
+      checkbox.checked = source.checked;
+}
