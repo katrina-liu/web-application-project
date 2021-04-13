1ce6e936da98c0ac748affc2dd747a475bb6d16b
Katrina Liu <xiaol3@andrew.cmu.edu>
2021-04-13 19:55:23 +0800

    Add check box total price

--- ecommerce_platform/static/js/base.js
+++ ecommerce_platform/static/js/base.js
@@ -4,6 +4,22 @@ function nav_drop_down(){
 
 function toggle(source) {
     checkboxes = document.getElementsByName('available_product');
-    for(var checkbox in checkboxes)
-      checkbox.checked = source.checked;
+     for(var i=0, n=checkboxes.length;i<n;i++) {
+    checkboxes[i].checked = source.checked;
+  }
+     selectProduct();
 }
+
+
+function selectProduct() {
+    checkboxes = document.getElementsByName('available_product');
+    var totalPrice = 0;
+    for(var i=0, n=checkboxes.length;i<n;i++) {
+        if (checkboxes[i].checked === true){
+            totalPrice += parseInt((checkboxes[i]).value);
+        }
+    }
+    var price = document.getElementById("id_total_price")
+    price.innerText= totalPrice
+}
+
