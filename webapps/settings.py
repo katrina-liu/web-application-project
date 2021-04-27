92bf303b2185655431ae446b1c2887052bb69006
Ubuntu <ubuntu@ip-172-31-22-140.us-east-2.compute.internal>
2021-04-27 18:10:55 +0000

    Replaced secret key

--- webapps/settings.py
+++ webapps/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from configparser import ConfigParser
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x#)2qs93tqor#gcbbhjbg)xw+yhakv@sd_+8hrtthuwi%%&#r$'
+CONFIG = ConfigParser()
+CONFIG.read(os.path.join(BASE_DIR, 'config.ini'))
+SECRET_KEY = CONFIG.get('Django', 'Secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
